bebc6e1c39c9e5a60c6bbff2ad3b987fcadcdd3f
ApProGrAMER <approgramer@gmail.com>
2016-04-16 22:41:49 +0530

    Update default.js

--- config/env/default.js
+++ config/env/default.js
@@ -23,7 +23,7 @@ module.exports = {
     secure: false
   },
   // sessionSecret should be changed for security measures and concerns
-  sessionSecret: process.env.SESSION_SECRET || 'peperfrybeef',
+  sessionSecret: 'peperfrybeef',
   // sessionKey is set to the generic sessionId key used by PHP applications
   // for obsecurity reasons
   sessionKey: 'sessionId',
